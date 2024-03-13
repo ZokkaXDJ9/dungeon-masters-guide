4a61dfadeb31fd48fdb2e59e91f284e4a2d6a8fd
Bahamut <leander.volprich@web.de>
2024-03-13 22:29:12 +0100

    This is fine

--- src/components/NPCDetails.jsx
+++ src/components/NPCDetails.jsx
@@ -1,15 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { auth, db } from '../firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore'; // Make sure these are imported
 
 const NPCDetails = () => {
   const { npcId } = useParams();
   const [npc, setNpc] = useState(null);
 
   useEffect(() => {
-    // Assuming NPCs are stored in session storage
-    const storedNpcs = JSON.parse(sessionStorage.getItem('npcs')) || [];
-    const foundNpc = storedNpcs.find(npc => npc.id === npcId);
-    setNpc(foundNpc);
+    const fetchNpcDetails = async () => {
+      if (auth.currentUser) {
+        // Fetch NPC details from Firestore
+        const npcRef = doc(db, "npcs", npcId);
+        const npcSnap = await getDoc(npcRef);
+
+        if (npcSnap.exists()) {
+          setNpc({ id: npcSnap.id, ...npcSnap.data() });
+        } else {
+          console.log("No such NPC!");
+        }
+      } else {
+        // Fetch NPC details from session storage
+        const storedNpcs = JSON.parse(sessionStorage.getItem('npcs')) || [];
+        const foundNpc = storedNpcs.find(npc => npc.id === npcId);
+        setNpc(foundNpc);
+      }
+    };
+
+    fetchNpcDetails();
   }, [npcId]);
 
   if (!npc) {
